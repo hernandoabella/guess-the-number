00957dd26737f251fe5dffa6b6c1cd55969dda20
Hernando Abella <hernandoabella@gmail.com>
2023-01-07 13:13:41 -0500

    update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// GUESS THE NUMBER GAME STEP BY STEP EXPLANATION
-//     ~ Hitik Saini.
+// ADIVINA EL NÚMERO
+
+
 //Generating a random number from 1-500
-let randomNumber = parseInt((Math.random()*500)+1);
+let randomNumber = parseInt((Math.random() * 500) + 1);
 //make variables
 const submit = document.querySelector('#user_guess');
 const userInput = document.querySelector('#guessField');
@@ -82,8 +83,8 @@ function endGame(){
     //disable the submit button
     submit.setAttribute('disabled', '');
     //Display Start new Game Button
-          p.classList.add('button');
-          p.innerHTML = `<h1 id="newGame">PLAY AGAIN</h1>`
+        p.classList.add('button');
+        p.innerHTML = `<h1 id="newGame">PLAY AGAIN</h1>`
     startOver.appendChild(p);
     playGame = false;
     newGame();
